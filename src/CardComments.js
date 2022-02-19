5081ad6b2acffe5947c2a6a23b10c8c9efeb98a4
Alexkawai <leha.iuganov@yandex.ru>
2022-02-19 13:29:10 +0300

    2 files modified

--- src/CardComments.js
+++ src/CardComments.js
@@ -1,17 +1,32 @@
 import React from 'react';
- const CardComments = (props) => {
+const CardComments = (props) => {
   // получаем обработчик закрытия окна
   return (
     <div className="modal">
-      <div>
       <div className="insideModal">
-      <ul>
-      {props.comments.map((item,key)=>(<li key={key}>{item.body}</li>))}
-      </ul>
-      <button onClick={props.onModalClose}>Close Modal</button>
-      </div>
+
+<div class="cl-btn-2" onClick={props.onModalClose}>
+    <div>
+        <div class="leftright"></div>
+        <div class="rightleft"></div>
+        <span class="close-btn">закрыть</span>
+    </div>
+</div>
+        {props.comments.map((item, key) => (
+          <div key={key}>
+            <textarea>{item.body}</textarea>
+            <a>Изменить</a>
+            <a>Удалить</a>
+          </div>
+        ))}
+        <div>
+          <textarea>{item.body}</textarea>
+          <a>Добавить</a>
+          <button onClick={props.onModalClose}>Закрыть</button>
+        </div>
+        
       </div>
     </div>
   );
 };
-export default CardComments
+export default CardComments;
