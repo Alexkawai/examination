6eaf9de13884a51681b825f6f7a70ee7f009e309
Alexkawai <leha.iuganov@yandex.ru>
2022-02-19 13:19:37 +0300

    2 files modified

--- src/CardComments.js
+++ src/CardComments.js
@@ -4,10 +4,12 @@ import React from 'react';
   return (
     <div className="modal">
       <div>
+      <div className="insideModal">
       <ul>
       {props.comments.map((item,key)=>(<li key={key}>{item.body}</li>))}
       </ul>
       <button onClick={props.onModalClose}>Close Modal</button>
+      </div>
       </div>
     </div>
   );
